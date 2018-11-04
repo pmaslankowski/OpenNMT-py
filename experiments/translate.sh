cca2d0f2bb08fda2e0f64de654d085098d85e86e
Piotr Maślankowski <pmaslankowski@gmail.com>
2018-11-04 20:02:08 +0100

    Added SentencePiece tokenization. Translation now works.

--- experiments/translate.sh
+++ experiments/translate.sh
@@ -1,2 +1,3 @@
-../tools/tokenizer.perl -a -no-escape -l en -q  < $1 > $1.atok
+spm_encode --model=../data/transformer/sentencepiece.model < $1 > $1.atok
 python ../translate.py -model ../data/transformer/averaged-10-epoch.pt -src $1.atok -output $1_output.atok -verbose
+rm *.atok
