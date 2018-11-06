b5556ec53f8f892948bf98022c8535d8881571ea
Piotr Maślankowski <pmaslankowski@gmail.com>
2018-11-06 18:18:59 +0100

    Refactoring of main script

--- research/main.py
+++ research/main.py
@@ -2,16 +2,24 @@ from research.scorer import Scorer
 from research.utils import load_vocabulary, Tokenizer
 
 if __name__ == '__main__':
-    scorer = Scorer()
-    score = scorer.score_texts('I think that machine translation is very interesting subject.',
-                         'Ich denke, dass maschinelle Übersetzung ein sehr interessantes Thema ist.')
-    print(score)
-
-
     vocab = load_vocabulary()
     tokenizer = Tokenizer()
-    english_tokens = tokenizer.tokenize('I think that machine translation is very interesting subject.')
-    german_tokens = tokenizer.tokenize('Ich denke, dass maschinelle Übersetzung ein sehr')
+    scorer = Scorer()
+
+    english_sentence = 'I think that machine translation is very interesting subject.'
+    german_translation_from_google = 'Ich denke, dass maschinelle Übersetzung ein sehr interessantes Thema ist.'
+
+    score = scorer.score_texts(english_sentence, german_translation_from_google)
+    print('Translation scoring')
+    print('English sentence:', english_sentence)
+    print('Translation from google translate to score:', german_translation_from_google)
+    print('Log likehood of translation:', score[0])
+    print()
+
+    unfinished_translation = 'Ich denke, dass maschinelle Übersetzung ein sehr'
+    english_tokens = tokenizer.tokenize(english_sentence)
+    german_tokens = tokenizer.tokenize(unfinished_translation)
     next_word_probs = scorer.next_word_probabilities([english_tokens], [german_tokens])
-    val, ind = next_word_probs.topk(1)
-    print(vocab.itos[ind.view(-1)])
+    val, ind = next_word_probs.topk(3)
+    print('Unfinished translation:', unfinished_translation )
+    print('Top 3 next tokens: ', vocab.itos[ind[0,0].view(-1)], vocab.itos[ind[0,1].view(-1)], vocab.itos[ind[0,2].view(-1)])
