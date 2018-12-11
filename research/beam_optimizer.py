773af14d19f123a1ca503c01e31da77262ae3140
Piotr Maślankowski <pmaslankowski@gmail.com>
2018-12-11 13:15:27 +0100

    BeamOptimizer - bug fix

--- research/beam_optimizer.py
+++ research/beam_optimizer.py
@@ -19,23 +19,24 @@ class BeamOptimizer(BaseOptimizer):
         replicated_english_tok_seq = [self.english_tok_seq] * n_beams_left
         init_probs, init_idxs = self.scorer.next_word_probabilities([self.english_tok_seq], [[]])[0].topk(n_beams_left)
         topk_probs = init_probs.unsqueeze(1)
-        topk_states = self.vocab.itos[init_idxs][:, np.newaxis]
+        topk_states = self.vocab.itos[init_idxs.detach().numpy()][:, np.newaxis]
         for _ in range(self.max_steps):
             next_state_probs = self.scorer.next_word_probabilities(replicated_english_tok_seq, topk_states)
             beam_probs = next_state_probs + topk_probs
             probs_flattened = beam_probs.view(-1)
             flat_probs, flat_idxs = probs_flattened.topk(n_beams_left)
-            beam_idxs = flat_idxs / self.vocab_size
+            flat_idxs = flat_idxs.detach().numpy()
+            beam_idxs = flat_idxs // self.vocab_size
             word_idxs = flat_idxs % self.vocab_size
             topk_probs = flat_probs.unsqueeze(1)
             topk_states = np.hstack((topk_states[beam_idxs], self.vocab.itos[word_idxs][:, np.newaxis]))
 
-            finished_beams_idx = (word_idxs == self.EOS).nonzero()
+            finished_beams_idx = np.flatnonzero(word_idxs == self.EOS)
             n_finished_now = len(finished_beams_idx)
             if n_finished_now > 0:
-                finished_beams += topk_states[finished_beams_idx.view(-1)].reshape(n_finished_now, -1).tolist()
+                finished_beams += topk_states[finished_beams_idx].reshape(n_finished_now, -1).tolist()
                 finished_beams_probs = np.concatenate((finished_beams_probs, topk_probs[finished_beams_idx].view(-1).detach().numpy()))
-                not_finished_beams_idx = (word_idxs != self.EOS).nonzero().view(-1)
+                not_finished_beams_idx = np.flatnonzero(word_idxs != self.EOS)
                 topk_probs = topk_probs[not_finished_beams_idx]
                 topk_states = topk_states[not_finished_beams_idx]
                 n_beams_left -= n_finished_now
